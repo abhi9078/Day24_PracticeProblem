ea37f2e14a45f0dae16973d80e57c666bd741fb6
Abhilash <abhilashmeher1234@gmail.com>
2022-04-10 10:58:51 +0530

    Added program files for UC_13

---
+++ LambdaFunctionValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LambdaFunctionValidation
+{
+    internal class Program
+    {
+
+        //Driver Method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Using Lambda Expression");
+
+
+            LambdaExpression lambdaExpression = new LambdaExpression();
+            lambdaExpression.Validate();
+
+        }
+    }
+}
